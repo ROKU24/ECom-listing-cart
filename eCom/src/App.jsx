5fe0395f0bcfc934b0885a68afaf195d5249fd0e
ROKU24 <96464268+ROKU24@users.noreply.github.com>
2024-11-19 08:24:27 +0000

    added cart svg

--- eCom/src/App.jsx
+++ eCom/src/App.jsx
@@ -14,10 +14,24 @@ const App = () => {
 <div className="container mx-auto px-4 py-4">
 <div className="flex justify-between items-center">
 <Link to="/" className="text-2xl font-bold text-blue-600">
-                 E-Commerce Store
+                 E Store
 </Link>
-<Link to="/cart" className="text-lg hover:text-blue-600">
-                 Cart
+{/* <Link to="/cart" className="text-lg hover:text-blue-600"> */}
+<Link to="/cart" className=" ">
+<svg
+ role="img"
+ xmlns="http://www.w3.org/2000/svg"
+ viewBox="0 0 24 24"
+ width="24px"
+ height="24px"
+>
+<path
+   fill="#285ff4"
+   d="M21 4H2v2h2.3l3.521 9.683A2.004 2.004 0 0 0 9.7 17H18v-2H9.7l-.728-2H18c.4 0 .762-.238.919-.606l3-7A.998.998 0 0 0 21 4"
+ />
+<circle cx="10.5" cy="19.5" r="1.5" fill="#285ff4" />
+<circle cx="16.5" cy="19.5" r="1.5" fill="#285ff4" />
+</svg>
 </Link>
 </div>
 </div>
